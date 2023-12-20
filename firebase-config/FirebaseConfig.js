ef4c003c0b1a1ee260a3c3ee8163fe54aad136d6
Umair <umairmunawar536@gmail.com>
2023-12-20 13:44:37 +0500

    firebase key protected

--- firebase-config/FirebaseConfig.js
+++ firebase-config/FirebaseConfig.js
@@ -8,12 +8,6 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    // apiKey: "AIzaSyBn7eeQRzs0zxIyM1ZzPhoUgVMVdbl01mo",
-    // authDomain: "react-chatapp-bf0b1.firebaseapp.com",
-    // projectId: "react-chatapp-bf0b1",
-    // storageBucket: "react-chatapp-bf0b1.appspot.com",
-    // messagingSenderId: "54704165313",
-    // appId: "1:54704165313:web:3ebe1e2bba228adda1df40"
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
